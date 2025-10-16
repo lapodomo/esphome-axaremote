26f81c0a901fd2dcde206a2ec6d449532a56bc26
lapodomo <46632570+lapodomo@users.noreply.github.com>
2025-10-16 19:44:24 +0200

    Update cover.cpp

--- components/axaremote/cover/cover.cpp
+++ components/axaremote/cover/cover.cpp
@@ -392,7 +392,7 @@ AXAResponseCode AXARemoteCover::send_cmd_(std::string &cmd, std::string &respons
       delay(1);  // Korte pauze als geen data om CPU druk te verminderen
     }
 
-		if (millis() - now > 100) {
+		if (millis() - now > 500) {
 			ESP_LOGE(TAG, "Timeout while waiting for response!");
 			return AXAResponseCode::Invalid;
 		}
